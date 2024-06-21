22cd300eb543084671f02260a762cab99415454c
inbar_zafar <inbar.zafar@s.afeka.ac.il>
2024-06-21 19:00:39 +0300

    inbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,84 +1,84 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import "../styles/home.css"
+import "../styles/home.css";
 
 function Home(){
-       
     return(
-    <>
-        <h2>Home</h2>
-        <img src="src/assets/Original on transparent.png" alt="FinRise Logo" class = "logo" />
-        <div class="custom-text">
-            <p>Empowers you to track income and expenses, create invoices and payment orders,<br />
-                manage your cash flow, prepare financial reports, and much more.<br />
-                This easy-to-use and powerful tool puts you in control of your finances and helps you achieve your business goals.
-            </p>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <Box
-        height={150}
-        width={300}
-        my={4}
-        mx={2}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="space-between"
-        marginTop={50}
-        gap={4}
-        p={2}
-        sx={{ 
-          border: '2px solid grey',
-          textAlign: 'center' // Align text in the center
-        }}
-      >
-        <h2 sx={{ color: 'blue', fontFamily: 'Arial' }}>Track Income and Expenses</h2>
-        <p>Monitor your financial performance with real-time tracking of income and expenses.</p>
-      </Box>
+        <>
+            <h2>Home</h2>
+            <img src="src/assets/Original on transparent.png" alt="FinRise Logo" className="logo" />
+            <div className="custom-text">
+                <p>Empowers you to track income and expenses, create invoices and payment orders,<br />
+                    manage your cash flow, prepare financial reports, and much more.<br />
+                    This easy-to-use and powerful tool puts you in control of your finances and helps you achieve your business goals.
+                </p>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Box
+                    height={200}
+                    width={300}
+                    my={4}
+                    mx={2}
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    marginTop={50}
+                    gap={4}
+                    p={2}
+                    sx={{ 
+                        border: '2px solid grey',
+                        textAlign: 'center' // Align text in the center
+                    }}
+                >
+                    <Typography variant="h5" sx={{ color: '#0eba97', fontFamily: 'Arial',fontWeight: 'bold' }}>Track Income and Expenses</Typography>
+                    <Typography variant="body1">Monitor your financial performance with real-time tracking of income and expenses.</Typography>
+                </Box>
 
-      <Box
-        height={150}
-        width={300}
-        my={4}
-        mx={2}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="space-between"
-        marginTop={50}
-        gap={4}
-        p={2}
-        sx={{ 
-          border: '2px solid grey',
-          textAlign: 'center' // Align text in the center
-        }}
-      >
-        <h2 sx={{ color: 'green', fontFamily: 'Verdana' }}>Create Invoices</h2>
-        <p>Generate professional invoices quickly and easily, and keep your billing organized.</p>
-      </Box>
+                <Box
+                    height={200}
+                    width={300}
+                    my={4}
+                    mx={2}
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    marginTop={50}
+                    gap={4}
+                    p={2}
+                    sx={{ 
+                        border: '2px solid grey',
+                        textAlign: 'center' // Align text in the center
+                    }}
+                >
+                    <Typography variant="h5" sx={{ color: '#0eba97', fontFamily: 'Arial',fontWeight: 'bold' }}>Create Invoices</Typography>
+                    <Typography variant="body1">Generate professional invoices quickly and easily, and keep your billing organized.</Typography>
+                </Box>
 
-      <Box
-        height={150}
-        width={300}
-        my={4}
-        mx={2}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="space-between"
-        marginTop={50}
-        gap={4}
-        p={2}
-        sx={{ 
-          border: '2px solid grey',
-          textAlign: 'center' // Align text in the center
-        }}
-      >
-        <h2 sx={{ color: 'red', fontFamily: 'Times New Roman' }}>Financial Reports</h2>
-        <p>Prepare detailed financial reports to understand your business better and make informed decisions.</p>
-      </Box>
-    </div>
+                <Box
+                    height={200}
+                    width={300}
+                    my={4}
+                    mx={2}
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    marginTop={50}
+                    gap={4}
+                    p={2}
+                    sx={{ 
+                        border: '2px solid grey',
+                        textAlign: 'center' // Align text in the center
+                    }}
+                >
+                    <Typography variant="h5" sx={{ color: '#0eba97', fontFamily: 'Arial' ,fontWeight: 'bold'}}>Financial Reports</Typography>
+                    <Typography variant="body1">Prepare detailed financial reports to understand your business better and make informed decisions.</Typography>
+                </Box>
+            </div>
  
         
       {/*  <div id = "services" class="boxes-container">
