a175ac1f888bbc8464207b933b75b55dea7b235b
Yosi Ben Shushan <Yosi.Ben.shushan1@s.afeka.ac.il>
2024-06-24 15:55:48 +0300

    added CustomerList (Page)

---
+++ src/pages/CustomerList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TableCustomerList from "../components/TableCustomersList";
+
+//inbar this is the new page you asked for :)
+function CustomerList() {
+  return (
+    <>
+      <TableCustomerList />
+    </>
+  );
+}
+
+export default CustomerList;
